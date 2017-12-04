46d8be5a5520128479ec0cd864624c307374a08c
lucafedri <luca.fedrizzi@belka.us>
2017-12-04 15:57:04 +0100

    Fixing carousel in home

--- js/main.js
+++ js/main.js
@@ -2,17 +2,43 @@
 ---
 '---';
 
-$('.carousel__home-slider').slick({
-  dots: false,
-  controls: false,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  centerMode: false,
-  variableWidth: true,
-  arrows: false,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  infinite: true,
-  speed: 500
-});
+
+$(document).ready(() => {
+
+  $('.carousel__home-slider_1').slick({
+    dots: false,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: false,
+    variableWidth: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+    speed: 500
+  });
+
+  $('.carousel__home-slider_2').slick({
+    dots: false,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: false,
+    variableWidth: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    infinite: true,
+    speed: 500
+  });
+  
+  $('[data-fancybox="gallery"]').not($('.slick-cloned')).fancybox({
+    buttons : [
+      'slideShow',
+      'fullScreen',
+      'thumbs',
+      'share',
+      'close'
+    ]
+  });
+
+})
