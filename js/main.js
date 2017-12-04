402bac1c5cde9b97988e4ce8ea9e35bb62aadfb8
lucafedri <luca.fedrizzi@belka.us>
2017-12-04 11:58:25 +0100

    Fixing photos and scroll speed

--- js/main.js
+++ js/main.js
@@ -10,5 +10,9 @@ $('.carousel__home-slider').slick({
   slidesToShow: 1,
   centerMode: false,
   variableWidth: true,
-  arrows: false  
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  infinite: true,
+  speed: 500  
 });
