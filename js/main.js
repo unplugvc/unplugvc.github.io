f282869c69f2420de643a8aff7da0282814c1c1d
lucafedri <luca.fedrizzi@belka.us>
2017-12-20 10:36:51 +0100

    VR-48

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(document).ready(() => {
   $('[data-fancybox="gallery"]')
     .not($('.slick-cloned'))
     .fancybox({
-      loop : true,
+      loop: true,
       buttons: ['close']
     });
 
@@ -277,3 +277,40 @@ function playVideo() {
     }
   }
 }
+
+/**
+ * Smooth scroll on link click
+ */
+$('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    if (
+      location.pathname.replace(/^\//, '') ==
+        this.pathname.replace(/^\//, '') &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate(
+          {
+            scrollTop: target.offset().top
+          },
+          1000,
+          function() {
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(':focus')) {
+
+              return false;
+            } else {
+              $target.attr('tabindex', '-1');
+              $target.focus();
+            }
+          }
+        );
+      }
+    }
+  });
